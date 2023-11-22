8074f0ed891bd7fd56687680502efaffde9acc1d
seongjin96 <w8348@naver.com>
2023-11-22 17:27:45 +0900

    header styling

--- vue-news/src/App.vue
+++ vue-news/src/App.vue
@@ -16,5 +16,8 @@ export default {
 </script>
 
 <style>
-
+body {
+  padding: 0;
+  margin: 0;
+}
 </style>
--- vue-news/src/components/ToolBar.vue
+++ vue-news/src/components/ToolBar.vue
@@ -1,17 +1,23 @@
 <template>
-  <div>
-    <router-link to="/news">News</router-link>
-    <router-link to="/ask">Ask</router-link>
+  <div class="header">
+    <router-link to="/news">News</router-link> |
+    <router-link to="/ask">Ask</router-link> |
     <router-link to="/jobs">Jobs</router-link>
   </div>
 </template>
 
-<script>
-export default {
-
+<!-- scoped: 해당 Component에만 적용되는 스타일 -->
+<style scoped>
+.header {
+  color: white;
+  background-color: #42b883;
+  display: flex;
+  padding: 8px;
 }
-</script>
-
-<style>
-
+.header .router-link-exact-active {
+  color: #35495e;
+}
+.header a {
+  color: white;
+}
 </style>
