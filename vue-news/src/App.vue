04a845f8017467f4a9dfa450186ea1b6087996d5
seongjin96 <w8348@naver.com>
2023-11-22 14:29:38 +0900

    off eslint setting

--- vue-news/src/App.vue
+++ vue-news/src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    hello
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  methods: {
+    fetchData() {
+      console.log('hello');
+    }
   }
 }
 </script>
