947e67883b80b42bf1c6c0390fa3043fc72a48e7
seongjin96 <w8348@naver.com>
2023-11-29 09:48:29 +0900

    set actions & mutations

--- vue-news/src/store/index.js
+++ vue-news/src/store/index.js
@@ -1,7 +1,25 @@
 import { createStore } from 'vuex';
+import { fetchNewsList } from '../api/index.js';
 
 export const store = new createStore({
   state: {
     news: []
+  },
+  mutations: {
+    SET_NEWS(state, news) {
+      state.news = news;
+    }
+  },
+  actions: {
+    FETCH_NEWS(context) {
+      fetchNewsList()
+        .then(response => {
+          console.log(response.data);
+          context.commit('SET_NEWS', response.data);
+        })
+        .catch(error => {
+          console.log(error);
+        })
+    }
   }
 });
--- vue-news/src/views/NewsView.vue
+++ vue-news/src/views/NewsView.vue
@@ -1,28 +1,25 @@
 <template>
   <div>
-    <div v-for="user in users">{{ user.title }}</div>
+    <div v-for="user in this.$store.state.news">{{ user.title }}</div>
   </div>
 </template>
 
 <script>
-import { fetchNewsList } from '../api/index.js';
+// import { fetchNewsList } from '../api/index.js';
 
 export default {
-  data() {
-    return {
-      users: []
-    }
-  },
   created() {
-    var vm = this;
-    fetchNewsList()
-      .then(function(response) {
-        console.log(response);
-        vm.users = response.data;
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
+    this.$store.dispatch('FETCH_NEWS');
+
+    // fetchNewsList()
+    //   .then(response => {
+    //     console.log(response);
+
+    //     this.users = response.data;
+    //   })
+    //   .catch(function(error) {
+    //     console.log(error);
+    //   });
   },
 }
 </script>
