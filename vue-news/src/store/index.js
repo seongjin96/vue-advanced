c372bb37285e03bcf991727d766c060999269364
seongjin96 <w8348@naver.com>
2024-01-08 11:22:29 +0900

    스토어 속성 모듈화

--- vue-news/src/store/index.js
+++ vue-news/src/store/index.js
@@ -1,5 +1,6 @@
 import { createStore } from 'vuex';
-import { fetchNewsList, fetchAskList, fetchJobsList } from '../api/index.js';
+import mutations from './mutations.js';
+import actions from './actions.js';
 
 export const store = new createStore({
   state: {
@@ -12,45 +13,6 @@ export const store = new createStore({
       return state.ask;
     }
   },
-  mutations: {
-    SET_NEWS(state, news) {
-      state.news = news;
-    },
-    SET_ASK(state, ask) {
-      state.ask = ask;
-    },
-    SET_JOBS(state, jobs) {
-      state.jobs = jobs;
-    }
-  },
-  actions: {
-    FETCH_NEWS(context) {
-      fetchNewsList()
-        .then(response => {
-          console.log(response.data);
-          context.commit('SET_NEWS', response.data);
-        })
-        .catch(error => {
-          console.log(error);
-        })
-    },
-    FETCH_ASK({ commit }) {
-      fetchAskList()
-        .then(({ data }) => {
-          commit('SET_ASK', data);
-        })
-        .catch(error => {
-          console.log(error);
-        })
-    },
-    FETCH_JOBS({ commit }) {
-      fetchJobsList()
-        .then(({ data }) => {
-          commit('SET_JOBS', data);
-        })
-        .catch(error => {
-          console.log(error);
-        })
-    }
-  }
+  mutations,
+  actions,
 });
