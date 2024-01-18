e6e2509ef0f433d985af7b84c02ba63ecbca53a9
seongjin96 <w8348@naver.com>
2024-01-18 12:20:16 +0900

    사용자 ㅋí프로필 컴포넌트 데이터 등록

--- vue-news/src/views/UserView.vue
+++ vue-news/src/views/UserView.vue
@@ -1,22 +1,28 @@
 <template>
   <div>
-    <p>name : {{ userInfo.id }}</p>
-    <p>karma : {{ userInfo.karma }}</p>
-    <p>created : {{ userInfo.created }}</p>
+    <user-profile :userInfo="userInfo"></user-profile>
+<!--    <user-profile></user-profile>-->
+
+<!--    <p>name : {{ userInfo.id }}</p>-->
+<!--    <p>karma : {{ userInfo.karma }}</p>-->
+<!--    <p>created : {{ userInfo.created }}</p>-->
   </div>
 </template>
 
 <script>
+import UserProfile from "../components/UserProfile.vue";
+
 export default {
+  components: {
+    UserProfile
+  },
   computed: {
     userInfo() {
       return this.$store.state.user;
     }
   },
   created() {
-    console.log(this.$route.params.id);
     const userName = this.$route.params.id;
-    // axios.get(`https://api.hnpwa.com/v0/user/${userName}.json`);
     this.$store.dispatch('FETCH_USER', userName);
   },
 }
