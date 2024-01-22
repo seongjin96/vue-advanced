11a03f43f349880ad424a0ceab708010b40148f6
seongjin96 <w8348@naver.com>
2024-01-22 16:17:24 +0900

    이벤트 버스를 이용한 스피너 컴포넌트 구현

--- vue-news/src/views/AskView.vue
+++ vue-news/src/views/AskView.vue
@@ -7,11 +7,12 @@
 <script>
 // import { mapState, mapGetters } from 'vuex';
 import ListItem from "../components/ListItem.vue";
+import bus from "@/utils/bus";
 
 export default {
   components: {
     ListItem,
-  }
+  },
   // computed: {
   //   ...mapGetters(
   //     // {
@@ -32,9 +33,18 @@ export default {
   //   //   return this.$store.state;
   //   // }
   // },
-  // created() {
-  //   this.$store.dispatch('FETCH_ASK');
-  // }
+  created() {
+    bus.emit('start:spinner');
+    setTimeout(() => {
+      this.$store.dispatch('FETCH_ASK')
+          .then(() => {
+            bus.emit('end:spinner');
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+    }, 3000);
+  }
 }
 </script>
 
