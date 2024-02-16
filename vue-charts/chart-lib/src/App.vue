323d4cc7bacbd36fe33013f6d41745f25fc772e8
seongjin96 <w8348@naver.com>
2024-02-16 14:14:14 +0900

    BarChart 컴포넌트화

--- vue-charts/chart-lib/src/App.vue
+++ vue-charts/chart-lib/src/App.vue
@@ -1,11 +1,19 @@
 <template>
   <div>
-    hello
+    <h1>Chart.js</h1>
+    <bar-chart></bar-chart>
   </div>
 </template>
 
 <script>
+import BarChart from "@/components/BarChart.vue";
+export default {
+  // 컴포넌트 속성 && 인스턴스 옵션
+  components: {
+    BarChart,
+  }
 
+}
 </script>
 
 <style scoped>
